e6a4ab35c872744388700aa773a8ef0ceb021da7
Addison Snelling <contact@asnell.io>
2022-03-17 06:34:00 -0500

    add install script to symlink files to home dir

---
+++ install.zsh
@@ -0,0 +1,15 @@
+#!/usr/bin/zsh
+
+dotfiles=${0:a:h}
+
+files_to_install=(
+  config/pythonrc.py
+  gitconfig
+  tmux.conf
+  vimrc
+  zshrc
+)
+for f in $files_to_install
+do
+  ln -s -r ${dotfiles}/${f} ~/.${f}
+done
