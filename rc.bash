2ca53bc6b943e6c0cd55d31c470516e0b13742ae
Addison Snelling <contact@asnell.io>
2022-09-08 03:44:16 -0500

    bashrc: sudo refresh, history file, globstar

--- rc.bash
+++ rc.bash
@@ -16,8 +16,9 @@ HISTCONTROL=ignoreboth
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=10000
+HISTFILESIZE=20000
+HISTFILE=~/.history/bash
 
 # check the window size after each command and, if necessary,
 # update the values of LINES and COLUMNS.
@@ -25,7 +26,7 @@ shopt -s checkwinsize
 
 # If set, the pattern "**" used in a pathname expansion context will
 # match all files and zero or more directories and subdirectories.
-#shopt -s globstar
+shopt -s globstar
 
 # make less more friendly for non-text input files, see lesspipe(1)
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
@@ -91,6 +92,9 @@ fi
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
+
+# sudo + refresh password timeout
+alias s='sudo -v; sudo '
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
